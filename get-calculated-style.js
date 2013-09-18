c7abfdafd66073dbe8ce853b1934f977c0bfbb0f
Joel Carras <joel.carras@bskyb.com>
2013-09-18 14:14:48 +0100

    'px' string removed and converted to float.

--- get-calculated-style.js
+++ get-calculated-style.js
@@ -2,12 +2,21 @@
 
   'use strict';
 
+  /**
+   * @getCalculatedStyle function
+   * @description Returns calculated style property of a DOM element.
+   * @param {element} DOM element to query.
+   * @returns {string} calculated style property you are looking for.
+   */
   function getCalculatedStyle( el, propName ) {
-    return el.currentStyle
+    var styleVal = el.currentStyle
       ? el.currentStyle[propName]
       : window.getComputedStyle
       ? document.defaultView.getComputedStyle(el, null).getPropertyValue(propName)
       : null;
+    if(styleVal){
+      return parseFloat(styleVal.replace('px', ''));
+    }
   }
 
   // transport
